83d533dba3c9239801482b39b65562e7ae1d1244
monoronjon sarker <monoronjons86@gmail.com>
2023-08-30 09:14:41 +0600

    morning-start

--- create-react-app/my-project/src/Card/Card.jsx
+++ create-react-app/my-project/src/Card/Card.jsx
@@ -3,12 +3,17 @@ import Title from './Title';
 import Price from './Price';
 
 const Card = () => {
+    const name="rokib"
+    const myObj={
+        name:"Hasan",
+        age:22
+    }
     return (
-        <div style={{
-            backgroundColor:"red"
-        }}>
+        <div>
             <Title></Title>
-            <Price></Price>
+            <Price name={name} age={myObj.age}></Price>
+            {/* <h2>{myObj.name}</h2>
+            <h2>{myObj.age}</h2> */}
         </div>
     );
 };
--- create-react-app/my-project/src/Card/Price.jsx
+++ create-react-app/my-project/src/Card/Price.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-const Price = () => {
+const Price = (props) => {
+    console.log(props)
     return (
         <div>
+            <h1>{props.name}{props.age}</h1>
             <p>$1200</p>
             <img src="./img/im.jpg" alt="" />
         </div>
