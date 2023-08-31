fdbb4e9b2967228b89db2f11bb1025b9ac8e2e5b
monoronjon sarker <monoronjons86@gmail.com>
2023-09-01 00:35:56 +0600

    use-state

--- reactjs-core-concept2/src/App.js
+++ reactjs-core-concept2/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 
 function App() {
 
   return (
     <div className="App">
-      <Country></Country>
+      {/* <Country></Country> */}
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
@@ -25,6 +26,23 @@ const deCount=()=>setCount(count -1)
       <button onClick={increaseCount}>Increase</button>
       <button onClick={deCount}>Decrease</button>
       </div>
+  )
+}
+function ExternalUsers(){
+  const[users,setUsers]=useState([])
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=>res.json())
+    .then(data=>setUsers(data))
+  },[])
+  return(
+    <div>
+      <h2>External users</h2>
+      {/* <p>{users.length}</p> */}
+      {
+        users.map(user=> <li>{user.name}</li>)
+      }
+    </div>
   )
 }
 // const products=[
