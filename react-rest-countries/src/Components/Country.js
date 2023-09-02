ec4686138d3fe3490268908bf20dd1e5d265de8c
monoronjon sarker <monoronjons86@gmail.com>
2023-09-03 01:06:03 +0600

    pass multiple data

--- react-rest-countries/src/Components/Country.js
+++ react-rest-countries/src/Components/Country.js
@@ -2,11 +2,24 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
+    console.log(props.country.area)
+    const{area,region,population,name}=props.country
     return (
         <div className='country'>
-            <h2>Country Name:{props.name}</h2>
+            {/* <h2>Country Name:{props.name}</h2>
             <h2>Population:{props.population}</h2>
-            <p><small>Area:{props.area}</small></p>
+            <p><small>Region:{props.region}</small></p>
+            <p><small>Area:{props.area}</small></p> */}
+            
+            {/* <h3>Country Name:{props.country.name.common}</h3> */}
+            {/* <p>Population:{props.country.population}</p> */}
+            {/* <p><small>Region:{props.country.region}</small></p> */}
+            {/* <p><small>Area:{props.country.area}</small></p> */}
+            <h3>Country Name:{name.common}</h3>
+            <p>Population:{population}</p>
+            <p><small>Region:{region}</small></p> 
+            <p><small>Area:{area}</small></p>
+
         </div>
     );
 };
