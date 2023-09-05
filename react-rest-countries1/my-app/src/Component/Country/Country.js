86953726d7f78dafb52bc13603c45bf9e95c2094
monoronjon sarker <monoronjons86@gmail.com>
2023-09-05 09:47:11 +0600

    practice end

--- react-rest-countries1/my-app/src/Component/Country/Country.js
+++ react-rest-countries1/my-app/src/Component/Country/Country.js
@@ -1,12 +1,34 @@
+// import React from 'react';
+// import './Country.css'
+
+// const Country = (props) => {
+//     return (
+//         <div className='country'>
+//             <h1>Country Name:{props.name}</h1>
+//             <p>Population:{props.population}</p>
+//             <p><small>Area:{props.area}</small></p>
+//         </div>
+//     );
+// };
+
+// export default Country;
+
 import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
+    console.log(props.country.population)
+    const{area,region}=props.country
     return (
         <div className='country'>
-            <h1>Country Name:{props.name}</h1>
-            <p>Population:{props.population}</p>
-            <p><small>Area:{props.area}</small></p>
+            {/* <h1>Country:{props.country.population}</h1>
+            <p>region:{props.country.region}</p>
+            <p><small>Area:{props.country.area}</small></p> */}
+
+            <p>Area:{area}</p>
+            <p>Region:{region}</p>
+
+
         </div>
     );
 };
