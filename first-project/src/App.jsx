ef6893256eb54bd3aed264be235a61b895f873fe
monoronjon sarker <monoronjons86@gmail.com>
2023-09-06 10:41:22 +0600

    first vite project

---
+++ first-project/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Devices></Devices>
+
+      
+    </>
+  )
+}
+function Car(){
+  return(
+    <div>hello</div>
+  )
+}
+export default App
---
+++ first-project/src/Component/Devices/Devices.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Devices = () => {
+    return (
+        <div>
+            <h1>Hello react lover</h1>
+        </div>
+    );
+};
+
+export default Devices;
