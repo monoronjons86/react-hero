02aff47a4cf39fd5b0daaaf22b2a9f965b08dee8
monoronjon sarker <monoronjons86@gmail.com>
2023-09-07 17:16:49 +0600

    local-server

--- react-storage-and-handler/src/Components/Cosmetic/Cosmetic.js
+++ react-storage-and-handler/src/Components/Cosmetic/Cosmetic.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Cosmetic.css'
+import { addToDb } from '../../utilities/fakedb';
 
 const Cosmetic = (props) => {
     const {name,price,id}=props.cosmetic
     const addToCart=(id)=>{
-        console.log('item added',id)
+        addToDb(id)
     }
     const addToCartWithParam=()=>addToCart(id)
     return (
@@ -16,8 +17,9 @@ const Cosmetic = (props) => {
             <h1>Buy this:{name}</h1>
             <h1>Price:{price}</h1>
             <p><small>Its has id:{id}</small></p>
-            <button onClick={addToCartWithParam}>Add to Cart</button>
-            <button onClick={()=>addToCart(id)}>Add to Cart:shortCut</button>
+            {/* <button onClick={addToCartWithParam}>Add to Cart</button>
+            <button onClick={()=>addToCart(id)}>Add to Cart:shortCut</button> */}
+            <button onClick={()=>addToCart(id)}>Add to th card</button>
         </div>
     );
 };
