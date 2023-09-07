a766ecf006ae6391d2a26151147238fd78b99e6f
monoronjon sarker <monoronjons86@gmail.com>
2023-09-07 14:43:25 +0600

    event-handelar-param add

--- react-storage-and-handler/src/Components/Cosmetic/Cosmetic.js
+++ react-storage-and-handler/src/Components/Cosmetic/Cosmetic.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './Cosmetic.css'
 
 const Cosmetic = (props) => {
     const {name,price,id}=props.cosmetic
+    const addToCart=(id)=>{
+        console.log('item added',id)
+    }
+    const addToCartWithParam=()=>addToCart(id)
     return (
-        <div>
+        <div className='product'>
             {/* <h1>Buy this:{props.cosmetic.name}</h1>
             {/* <h2>Price:{props.cosmetic.price}</h2> */}
             {/* <p>Price:{props.cosmetic.price}</p> */}
@@ -11,6 +16,8 @@ const Cosmetic = (props) => {
             <h1>Buy this:{name}</h1>
             <h1>Price:{price}</h1>
             <p><small>Its has id:{id}</small></p>
+            <button onClick={addToCartWithParam}>Add to Cart</button>
+            <button onClick={()=>addToCart(id)}>Add to Cart:shortCut</button>
         </div>
     );
 };
