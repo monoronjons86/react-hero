38e56bc66077eb9860c24e9b5be3d3cc8b8cb127
monoronjon sarker <monoronjons86@gmail.com>
2023-09-07 21:20:45 +0600

    local stortage

--- react-storage-and-handler/src/utilities/fakedb.js
+++ react-storage-and-handler/src/utilities/fakedb.js
@@ -1,15 +1,29 @@
 // use local storage to manage cart data
 const addToDb=id=>{
-    const quantity=localStorage.getItem(id);
+    let shoppingCart;
+     //get the shoping card
+     const storedCart=localStorage.getItem('shopping-cart')
+     if(storedCart){
+        shoppingCart=JSON.parse(storedCart)
+
+     }
+     else{
+        shoppingCart={}
+
+     }
+     //add quantity
+    const quantity=shoppingCart[id]
     if(quantity){
-        console.log('already exists')
-        const newQuantity=parseInt(quantity)+1
-        localStorage.setItem(id,newQuantity)
+        const newQuantity=quantity+1
+        shoppingCart[id]=newQuantity
+        // localStorage.setItem(id,newQuantity)
     }
     else{
-        console.log('new item')
-        localStorage.getItem(id,1)
+        shoppingCart[id]=1
+        // localStorage.setItem(id,1)
     }
+    localStorage.setItem('shopping-cart',JSON.stringify(shoppingCart))
+
   
 }
 export{addToDb};
