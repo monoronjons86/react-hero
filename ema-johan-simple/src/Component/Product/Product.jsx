d034b0724f992c0a93d0a85c38f38ae8353a314d
monoronjon sarker <monoronjons86@gmail.com>
2023-10-03 23:46:46 +0600

    addde

--- ema-johan-simple/src/Component/Product/Product.jsx
+++ ema-johan-simple/src/Component/Product/Product.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    const{img,name,seller,quantity,price}=props.product
+    const{img,name,seller,ratings,quantity,price}=props.product
     return (
-        <div className='product'>
+        <div className='product'> 
             <img src={img}/>
+           <div className='product-info'>
+           <h6 className='product-name'>{name}</h6>
+            <p>Price:${price}</p>
+            <p>Manufacturer:{seller}</p>
+            <p>Rating:{ratings}Stars</p>
+           </div>
         </div>
     );
 };
