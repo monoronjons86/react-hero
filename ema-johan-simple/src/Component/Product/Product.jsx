654beebac47eb04071bf0ae00f016a93b7a83aad
monoronjon sarker <monoronjons86@gmail.com>
2023-10-08 23:44:36 +0600

    mid night work

--- ema-johan-simple/src/Component/Product/Product.jsx
+++ ema-johan-simple/src/Component/Product/Product.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 
 const Product = (props) => {
@@ -17,7 +17,9 @@ const Product = (props) => {
             <p>Manufacturer:{seller}</p>
             <p>Rating:{ratings}Stars</p>
            </div>
-           <button onClick={ ()=>handleAddToCart(props.product)} className='btn-cart'>Add to cart</button>
+           <button onClick={ ()=>handleAddToCart(props.product)} className='btn-cart'>Add to cart
+           <FontAwesomeIcon icon={faShoppingCart} />
+           </button>
         </div>
     );
 };
